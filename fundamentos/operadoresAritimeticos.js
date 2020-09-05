d70ba20bc6d004a2f8a6b42d5da29dc71e2579eb
Rogerio Andre Queke Junior <junior_queke1@hotmail.com>
2020-09-05 09:47:53 -0300

    Aula 45 sobre operadores relacionais

---
+++ fundamentos/operadoresAritimeticos.js
@@ -0,0 +1,17 @@
+const [a, b, c, d] = [3, 1, 5, 15]
+
+const soma = a + b + c + d
+const subratracao = d - b
+const multiplicacao = a * b
+const divisao = d / a
+const modulo = a % 2
+
+console.log("resultado da soma é ", soma)
+console.log()
+console.log("resultado da subatração é ", subratracao)
+console.log()
+console.log("resultado da multiplicação é ", multiplicacao)
+console.log()
+console.log("resultado da divisão é ", divisao)
+console.log()
+console.log("resultado do módulo é ", modulo)
