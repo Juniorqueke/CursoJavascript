f41e147063a992faa468c5c05b2c6a106ad402de
Rogerio Andre Queke Junior <junior_queke1@hotmail.com>
2020-09-18 17:26:17 -0300

    Arquivos com a sintaxe da estrutura If, IfElse em JS

---
+++ controle/if1.js
@@ -0,0 +1,14 @@
+function boaNoticia(nota) {
+    if (nota >= 7) {
+        console.log('Aprovado, sua nota foi ' + nota)
+    }
+}
+
+boaNoticia(7.8)
+boaNoticia(5.3)
+
+function eVerdade(valor) {
+    if (valor) {
+        console.log('Verdade ' + valor)
+    }
+}
---
+++ controle/if2.js
@@ -0,0 +1,19 @@
+function teste1(num) {
+    if (num > 7)
+        console.log(num)
+
+    console.log('Final')
+}
+
+//teste1(6)
+//teste1(8)
+
+function teste2(numero) {
+    if (numero > 7); {
+        //Não colocar ; na definição de uma estrutura de controle
+        console.log(numero)
+    }
+}
+
+teste2(6)
+teste2(8)
